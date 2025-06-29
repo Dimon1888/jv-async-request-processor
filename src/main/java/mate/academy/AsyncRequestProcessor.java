4892f4e2014dfa23c49c4349e86823cac461de69
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-06-29 21:24:16 +0200

    Developed a Java application that asynchronously processes user requests using CompletableFuture and caching the results in a Map interface instance for fast retrieval in future requests.

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,44 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        UserData cachedData = cache.get(userId);
+        if (cachedData != null) {
+            System.out.println("Data received for user " + userId + " from the cache.");
+            return CompletableFuture.completedFuture(cachedData);
+        }
+        System.out.println("Asynchronous request processing for the user " + userId + " ....");
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                long currentTimestamp = System.currentTimeMillis();
+                String detalis = "Details for" + userId + " (obtained in "
+                        + currentTimestamp + ")";
+                UserData userData = new UserData(userId, detalis);
+
+                cache.put(userId, userData);
+                System.out.println("User data processing "
+                        + "and caching completed " + userId + ".");
+                return userData;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new IllegalStateException("Failed to process"
+                        + " request for user " + userId, e);
+            }
+        }, executor);
+
     }
 }
--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,11 +2,14 @@ package mate.academy;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         // Feel free to play with AsyncRequestProcessor in this main method if you want
-        ExecutorService executor = null; // Provide implementation that fits your needs
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        // Provide implementation that fits your needs
         AsyncRequestProcessor asyncRequestProcessor = new AsyncRequestProcessor(executor);
 
         // Simulating multiple concurrent requests
@@ -21,6 +24,24 @@ public class Main {
 
         // Wait for all futures to complete
         CompletableFuture.allOf(futures).join();
+
+        System.out.println("\n-- All initial requests have"
+                + " been processed. Resubmitting a request ---");
+        asyncRequestProcessor.processRequest("user1")
+                .thenAccept(userData -> System.out.println(" Repeated request"
+                        + " for user1: " + userData))
+                .join();
+
         executor.shutdown();
+
+        try {
+            if (!executor.awaitTermination(5, TimeUnit.SECONDS)) {
+                executor.shutdownNow();
+            }
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("\nThe application has completed its work. ");
     }
 }
